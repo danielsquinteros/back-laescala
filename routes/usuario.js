76ba8550f68db10b6eb394c49ac9b5a71a83eb15
Daniel Quinteros <danielsquinteros@gmail.com>
2020-02-14 13:07:37 -0300

    Create a first user the app

--- routes/usuario.js
+++ routes/usuario.js
@@ -5,7 +5,12 @@ import auth from '../middlewares/auth';
 
 const router = routerx();
 
-router.post('/add', auth.verifyAdministrador ,usuarioController.add);
+//Para partir el proyecto sacar el verify administador, como se muestra a continuación
+//Y crear usuario administrador a través de POSTMAN en el correo se adjunta la petición
+//Una vez creador el usuario administrador se agrega el verify administrador y se borra
+//la línea 12 de este código =>  (router.post('/add',usuarioController.add);
+router.post('/add',usuarioController.add);
+//router.post('/add', auth.verifyAdministrador ,usuarioController.add);
 router.get('/query', auth.verifyAdministrador,usuarioController.query);
 router.get('/list',auth.verifyAdministrador,usuarioController.list);
 router.put('/update', auth.verifyAdministrador,usuarioController.update);
